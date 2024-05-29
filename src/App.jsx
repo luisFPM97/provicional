bf1659255ce2a60885adf463ecfc321fa6f81c7c
luisFPM97 <134348310+luisFPM97@users.noreply.github.com>
2024-05-29 15:38:27 -0500

    update fecha

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,33 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import moment from 'moment'; // Importar librería Moment.js
+
+
 function App() {
   const [count, setCount] = useState(0)
+  const [fechaActual, setFechaActual] = useState(moment());
+const [fechaObjetivo, setFechaObjetivo] = useState(moment('2024-06-29 12:00:00'));
+
+
+
+ let date
+ let minutes
+ let segundos
+
+useEffect(() => {
+  let meses
+  const intervalId = setInterval(() => {
+    /*setFechaActual(moment());*/
+    date = new Date()
+    minutes = 60-date.getMinutes()
+    segundos = 60-date.getSeconds()
+    console.log(segundos)
+  }, 1000); // Actualizar cada segundo (1000 milisegundos)
+   // Limpiar el intervalo al desmontar el componente
+}, []);
 
   return (
     <>
@@ -27,6 +50,10 @@ function App() {
       <i class='bx bxs-error bx-flashing'></i>
       <span>Proximamente sitio web</span>
       </div>
+      <div className="tiempo-restante">
+      <span>Tiempo restante hasta el 29 de junio de 2024 a las 12 del mediodía:</span>
+      <span>{segundos} segundos</span>
+    </div>
     </>
   )
 }
